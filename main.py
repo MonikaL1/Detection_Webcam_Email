072bbf2055d632d8314b568047525c3629e88f84
Monika <lumimonika1@gmail.com>
2024-04-08 20:47:02 -1200

    Create images directory tov access photo. Send email when the object is detected in the cameray.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import cv2
 import time
 from emailing import send_email
 import glob
+import os
+from threading import Thread
+
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -10,6 +13,14 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+def clean_folder():
+    print("clean_folder function started")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("clean_folder function ended")
+
 
 while True:
 
@@ -43,7 +54,7 @@ while True:
 
 
             all_images = glob.glob("images/*.png")
-            index = int(len(all_images)/2)
+            index = int(len(all_images) / 2)
             image_with_object = all_images[index]
 
 
@@ -52,16 +63,23 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+
+
 
     print(status_list)
 
 
     cv2.imshow("Video", frame)
-
     key = cv2.waitKey(1)
 
     if key == ord("q"):
         break
 
 video.release()
+clean_thread.start()
